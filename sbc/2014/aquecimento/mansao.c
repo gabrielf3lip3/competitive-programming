f230293c80a079623fbfd59dc35ace69e6c879df
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-07-27 13:36:09 -0300

    add mansao exercise

---
+++ sbc/2014/aquecimento/mansao.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void descendenteMuda(int doors[], int size, int index) {
+
+	for(int i = 1; i <= size; i++) {
+
+		if((i > 0 && (i % index == 0)) || i == 0)
+			if(doors[i] == 1) doors[i] = 0;
+			else doors[i] = 1;
+	}
+}
+
+void showOpened(int doors[], int size) {
+	
+	for(int i = 1; i <= size; i++) {
+		if(doors[i] == 1)
+			printf("%d ", i);
+
+	}
+
+	putchar('\n');
+}
+
+int main() {
+
+	int num;
+	scanf("%d", &num);
+
+	int doors[num+1];
+	//Porta fechada: 0; porta aberta: 1;
+	for(int i = 1; i <= num; i++)
+		doors[i] = 0;
+
+
+	for(int i = 1; i <= num; i++)
+		descendenteMuda(doors, num, i);
+
+	showOpened(doors, num);
+
+	return 0;
+}

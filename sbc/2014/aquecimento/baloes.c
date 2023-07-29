5e568a11640797dc587be34329bd606239442a1e
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-07-28 22:12:49 -0300

    upgrade balao exercise

--- sbc/2014/aquecimento/baloes.c
+++ sbc/2014/aquecimento/baloes.c
@@ -13,8 +13,13 @@ int main() {
   int flechas = 0;
   for(int i = 0; i < qtde; i++) {
     if(i==0) flechas++;
-    else if(pos[i] - pos[i-1] != -1) flechas++;
-
+    else if(pos[i] - pos[i-1] != -1) {
+      flechas++;
+    
+      for(int j = i; j < qtde; j++) {
+        if(pos[j] - pos[i-1] == -1) flechas--;
+      }
+    }
   }
 
   printf("%d\n", flechas);

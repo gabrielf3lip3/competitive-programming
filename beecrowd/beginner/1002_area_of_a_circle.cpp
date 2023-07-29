026a5b15bc8be588f0d3aea8fc7b6fc1568a126f
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-07-29 01:15:53 -0300

    move balao exercise

---
+++ beecrowd/beginner/1002_area_of_a_circle.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+    double r, a;
+    std::cin >> r;
+    a = r*r*3.14159;
+    printf("A=%.4lf\n", a);
+}
---
+++ exercism/c/square_root.c
@@ -0,0 +1,5 @@
+#include <math.h>
+
+unsigned int square_root(unsigned int number) {
+    return pow(number, 0.5);
+}

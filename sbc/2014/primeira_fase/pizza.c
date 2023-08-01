2b676c73567f061705e713f8e0b81170893da0ca
gabrielfelipeassuncaodesouza <gabrielfelipeassuncaodesouza@gmail.com>
2023-08-01 13:36:56 -0300

    correct pizza exercise

--- sbc/2014/primeira_fase/pizza.c
+++ sbc/2014/primeira_fase/pizza.c
@@ -1,24 +1,63 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-char isPossible(int positions[], int circu, int size) {
-  if(circu < size) return 'N';
+char isPossible(int positions[], int qtde, int circu) {
 
-  for(int i = 1; i < size; i++) {
-    if(positions[i] - positions[i-1] < size) return 'N';
-  }
-  return 'S';
+	if(qtde > circu || circu % qtde != 0)
+		return 'N';
+
+	int comprimentoSetor = circu / qtde;
+	printf("Comprimento de cada setor: %d\n", comprimentoSetor);
+
+	int limits[qtde+1];
+	for(int i = 0; i <= qtde; i++) {
+		limits[i] = i*comprimentoSetor; // 0 4 8 e 12
+		printf("Limite %d: %d\n", i+1, limits[i]);
+	}
+	
+	for(int i = 0; i < circu; i++) {
+	
+		bool possible = false;
+
+		for(int j = 0; j < qtde; j++) {
+		
+			printf("Analisando se %d é maior que %d e menor que %d\n", positions[j], limits[j], limits[j+1]);	
+			if(positions[j] > limits[j] && positions[j] < limits[j+1]) {
+				printf("Está dentro do intervalo\n");
+				possible = true;
+			} else {
+				printf("Não está dentro do intervalo\n");
+				possible = false;
+				break;	
+			}
+		}
+	
+		if(possible) {
+			printf("Existe uma partição\n");
+			return 'S';
+		} else {
+			printf("Não existe uma combinação, deslocando os limites: \n");
+			for(int i = 0; i <=qtde; i++) {
+					limits[i]++;
+					printf("Limite %d: %d\n", i+1, limits[i]);
+			}
+		}
+	}
+
+	return 'N';
 }
 
 int main() {
 
-  int circu, num;
-  scanf("%d %d", &circu, &num);
+	int circu, qtde;
+	scanf("%d %d", &circu, &qtde);
 
-  int positions[num];
-  for(int i = 0; i < num; i++) {
-    scanf("%d", &positions[i]);
-  }
+	int positions[qtde];
+	for(int i = 0; i < qtde; i++) {
+		scanf("%d", &positions[i]);
+	}
 
-  char result = isPossible(positions, circu, num);
-  printf("%c\n", result);
+	char asnw = isPossible(positions, qtde, circu);
+	printf("%c\n", asnw);
+	return 0;
 }
